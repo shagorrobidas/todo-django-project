a65db2f9cb4e7f3e80d2b09c39ac1d097c98b60f
Shagor Robidas <roysagor88@gmail.com>
2024-12-05 09:14:44 +0600

    add list

--- tasks/views.py
+++ tasks/views.py
@@ -4,3 +4,10 @@ from .models import Task
 from .forms import TaskFrom
 
 # Create your views here.
+
+
+@login_required
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
